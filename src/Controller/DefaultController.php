08dc7bc61dc17b5d665f933be241ff489f3be0d2
Ahmet DOGRU <77503629+Yunis78@users.noreply.github.com>
2021-04-27 14:30:02 +0200

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,6 +13,7 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
+        $lol = 'lol';
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
