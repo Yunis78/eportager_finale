9641cd0fb4bc774135de4e3bff3cf12f5a299837
MelissaPas <mel.pascal@gmail.com>
2021-04-27 17:27:58 +0200

    homePage bootstrap

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'remixicon/fonts/remixicon.css'
 
 // start the Stimulus application
 // import './bootstrap';
