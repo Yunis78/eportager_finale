8ba74c7c7317f14e5455179a8bc81575582519c5
Naltpom <nathan.provost78990@gmail.com>
2021-05-02 17:14:12 +0200

    Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -62,7 +62,7 @@ class RegistrationController extends AbstractController
                     ->from(new Address('robotepotager@gmail.com', 'Acme mail bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
+                    ->htmlTemplate('components/pages/registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
 
